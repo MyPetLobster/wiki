53add1ef2271ec1a8b6aff7ae16b5a480b50066c
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-02-01 18:12:31 -0500

    Make modular, move REs to own functions

--- md_converter.py
+++ md_converter.py
@@ -7,8 +7,12 @@ paragraphs. You may find using regular expressions in Python helpful.
 '''
 # Function that uses regex to convert markdown to html
 def md_converter(content):
-    # Convert headers 
+    html_content = convert_headers(convert_bold(convert_ul(content)))
 
+    return html_content
+
+
+def convert_headers(content):
     # Do i need that \s at the end of this?
     content = re.sub(r'(?m)^(#{1,6})\s*(.+?)\s*$', r'<\1>\2</\1>', content)
     content = content.replace('<#>', '<h1>').replace('</#>', '</h1>')
@@ -17,13 +21,13 @@ def md_converter(content):
     content = content.replace('<####>', '<h4>').replace('</####>', '</h4>')
     content = content.replace('<#####>', '<h5>').replace('</#####>', '</h5>')
     content = content.replace('<######>', '<h6>').replace('</######>', '</h6>')
+    return content
 
-    # Convert boldface text
-    content = re.sub(r'(?m)\*\*(.+?)\*\*', r'<strong>\1</strong>', content)
+def convert_bold(content):
+    return re.sub(r'(?m)\*\*(.+?)\*\*', r'<strong>\1</strong>', content)
 
-    # Convert unordered lists
+def convert_ul(content):
     content = re.sub(r'(?m)^(\s*)?[-|*|+]\s*(.+?)\s*$', r'\1    <li>\2</li>', content)
-
     lines = content.splitlines()
     inside_ul = False
 
@@ -45,11 +49,10 @@ def md_converter(content):
 
     content = '\n'.join(lines)
 
-
-
     return content
 
-
+def convert_paragraphs(content):
+    content = re.sub(r'', r'', content)
 
 content = '''
 - test item one
