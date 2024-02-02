f23804a44a326dc4606cab2b8433ad362ee95377
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-02-02 13:32:17 -0500

    Move converter, start working on views

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . import md_converter
 from . import util
 
 
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    markdown_content = util.get_entry(title)
+    if not markdown_content:
+        return render(request, "encyclopedia/error.html", {
+            "error_message": "The requested page was not found."
+        })
+    
+    html_content = md_converter.md_converter(markdown_content)
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": html_content
+    })
