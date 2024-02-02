7dd3c36a5fc5a5969c22a2134d74852daa8b3423
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-02-02 14:54:39 -0500

    Implement create new page functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
+    path("new-entry", views.new, name="new"),
+    path("save", views.save, name="save"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,11 @@
+from django import forms
 from django.shortcuts import render
 from . import md_converter
 from . import util
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content", widget=forms.Textarea)
 
 
 def index(request):
@@ -39,3 +44,29 @@ def search(request):
     return render(request, "encyclopedia/search.html", {
         "entries": sub_matches
     })
+
+def new(request):
+    return render(request, "encyclopedia/new-entry.html", {
+        "form": NewEntryForm()
+    })
+
+def save(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = "# " + title + "\n\n" + form.cleaned_data["content"]
+            if util.get_entry(title):
+                return render(request, "encyclopedia/error.html", {
+                    "error_message": "An entry with this title already exists."
+                })
+            util.save_entry(title, content)
+            return entry(request, title)
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "error_message": "Invalid form."
+            })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "error_message": "Invalid request."
+        })
